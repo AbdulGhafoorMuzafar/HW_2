45225d79d9c019fd7c9b27ca97a1c3d9e49c2521
Abdul Ghafoor Muzafar <148848423+AbdulGhafoorMuzafar@users.noreply.github.com>
2023-10-24 06:11:54 -0700

    Add files via upload

---
+++ average _student_number.py
@@ -0,0 +1,9 @@
+name = input("Enter Your Name :")
+lastname = input("Enter Your LastName :")
+dars1 = float(input("Enter your score"))
+dars2 = float(input("Enter your score"))
+dars3 = float(input("Enter your score"))
+if(dars1+dars2+dars3)/3 >= 17 :
+    print("Greate !")
+elif (dars1+dars2+dars3)/3 <= 17 and (dars1+dars2+dars3)/3 >= 12 :
+    print("Normal !")

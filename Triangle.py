89d668329d45bb321a62cd0b50748c7d6f83b3f3
Abdul Ghafoor Muzafar <148848423+AbdulGhafoorMuzafar@users.noreply.github.com>
2023-10-27 10:05:08 -0700

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,7 +1,9 @@
+import math
+
 a = int(input("Enter First Number :"))
 b = int(input("Enter Second Number :"))
 c = int(input("Enter Third Number :"))
-if(a+b>c or b+c>a or a+c>b) :
+if(a+b>c and b+c>a and a+c>b) :
     print ("We can draw triangle with these numbers :",a,b,c)
 else :
     print("We can not draw triangle with these numbers : ",a,b,c)
